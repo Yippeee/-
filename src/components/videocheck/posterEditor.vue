103128edad23b515855c86dfae5cb20432923ca5
tangcong <tangcong@ipanel.cn>
2018-08-24 12:15:37 +0800

    大致搞成图片的上传和下载

--- src/components/videocheck/posterEditor.vue
+++ src/components/videocheck/posterEditor.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="posterEditor">
-    <!-- <image-editor
-      :options="{
-          systemType: 'news',
-          index: 1,
-      }" 
-      @callback="afterEditPoster">
-    </image-editor> -->
-    <canvas id="canvas">浏览器不支持HTML5</canvas>
-    <img src="../../assets/logo.png" alt="">
-    <img id="demo" alt="">
+    <img id="demo" alt="" style="width:100px;height:100px">
     <el-button @click="dialogVisible = true,nowStep = 1"></el-button>
     <el-dialog
       :visible.sync="dialogVisible"
@@ -47,8 +38,8 @@
           <div v-if="nowStep == 2">
             <span class="alter">批量上传前，请检查图片是否已按照规则命好名，如‘500x280’</span>
             <el-upload
+              ref="upload"
               class="el-upload2"
-              action="https://jsonplaceholder.typicode.com/posts/"
               list-type="picture-card"
               :on-remove="handleRemove"
               :on-change="change"
@@ -74,7 +65,7 @@
         <el-button v-show="nowStep == 1" type="primary" @click="nowStep = 5">截取帧画</el-button>
         <el-button v-show="nowStep == 1" type="primary" @click="nowStep = 2">本地上传</el-button>
         <el-button v-show="nowStep == 1" @click="dialogVisible = false">确认</el-button>
-        <el-button v-show="nowStep == 2||nowStep == 5" @click="nowStep++">下一步</el-button>
+        <el-button v-show="nowStep == 2||nowStep == 5" @click="nextStep">下一步</el-button>
         <el-button v-show="nowStep == 3||nowStep == 4||nowStep == 6" @click="nowStep = 1">完成</el-button>
         <el-button v-show="nowStep == 2||nowStep == 3||nowStep == 4||nowStep == 5||nowStep == 6" @click="dialogVisible = false">取消</el-button>
       </span>
@@ -82,74 +73,120 @@
   </div>
 </template>
 <script>
+import Axios from "axios";
+
 export default {
   data() {
     return {
-      steps : ['','海报','本地上传-上传图片','本地上传-设配尺寸','选择尺寸','截取帧画','截取帧画-设配尺寸','编辑','海报上传'],
-      nowStep : 1,
+      steps: [
+        "",
+        "海报",
+        "本地上传-上传图片",
+        "本地上传-设配尺寸",
+        "选择尺寸",
+        "截取帧画",
+        "截取帧画-设配尺寸",
+        "编辑",
+        "海报上传"
+      ],
+      nowStep: 1,
       title: "海报",
       dialogVisible: true,
-      dialogImageUrl: '',
-      dialogVisible2: true
+      dialogImageUrl: "",
+      dialogVisible2: true,
+      uploadData: {},
+      fileArray: []
     };
   },
   methods: {
     handleRemove(file, fileList) {
-      let nodes = document.getElementsByClassName('el-upload-list__item')
-      Array.from(nodes).forEach((element,index) => {
-        if (index < nodes.length-1) {
-          element.setAttribute('imgName',fileList[index].name.match(/([^;]*)\./)[1])
+      let nodes = document.getElementsByClassName("el-upload-list__item");
+      Array.from(nodes).forEach((element, index) => {
+        if (index < nodes.length - 1) {
+          element.setAttribute(
+            "imgName",
+            fileList[index].name.match(/([^;]*)\./)[1]
+          );
         }
       });
     },
-    change(){
-      this.$nextTick( () => {
-        console.log('change')
-        let dom = document.getElementsByClassName('el-upload__input')[0]
-        let imageName = dom.files[0].name.match(/([^;]*)\./)[1]
-        let nodes = document.getElementsByClassName('el-upload-list__item')
-        let node = nodes[nodes.length-1]
-        node.setAttribute('imgName',imageName)
-      }) 
+    change(file, fileList) {
+      this.$nextTick(() => {
+        console.log("change");
+        let dom = document.getElementsByClassName("el-upload__input")[0];
+        this.fileArray.push(dom.files[0])
+        let imageName = dom.files[0].name.match(/([^;]*)\./)[1];
+        let nodes = document.getElementsByClassName("el-upload-list__item");
+        let node = nodes[nodes.length - 1];
+        node.setAttribute("imgName", imageName);
+      });
     },
-    demo () {},
-    turnToActive (e) {
-      let target = e.target.nodeName
-      console.log(target.nodeName)
-      if(target == 'IMG'){
-        let a = this.$refs['firstStep'].getElementsByClassName('active')[0]
-        if(a){
-          a.className = ''
+    demo() {},
+    turnToActive(e) {
+      let target = e.target.nodeName;
+      console.log(target.nodeName);
+      if (target == "IMG") {
+        let a = this.$refs["firstStep"].getElementsByClassName("active")[0];
+        if (a) {
+          a.className = "";
         }
-        e.target.className = 'active'
-        console.log(e.target.className)
-      }else if(target == "I"){
-        this.nowStep = 4
+        e.target.className = "active";
+        console.log(e.target.className);
+      } else if (target == "I") {
+        this.nowStep = 4;
       }
+    },
+    nextStep() {
+      // this.nowStep++;
+      let formData = new FormData();
+      // let file = this.fileArray[0]
+      let url = ''
+      if(this.fileArray.length === 1){
+        url = 'http://192.168.21.29:8099/poster/poster/upload'
+      }else if(this.fileArray.length > 1){
+        url = 'http://192.168.21.29:8099/poster/posters/upload'
+      }
+      this.fileArray.forEach(item => {
+        formData.append("file", item);
+      })
+      formData.append("widthList", 400);
+      formData.append("heightList", 400);
+      formData.append("seriesId", 1);
+      formData.append("videoNumber", 1);
+
+      let config = {
+          headers:{'Content-Type':'multipart/form-data'}
+      }; 
+      Axios.post(url,formData,config).then((response) => {
+        console.log(response.data.msg)
+      })
     }
   },
-  watch:{
-    nowStep (v) {
-      this.title = this.steps[v]
+  watch: {
+    nowStep(v) {
+      this.title = this.steps[v];
     }
   },
   mounted() {
-    var canvas = document.getElementById("canvas")
-    let demo = document.getElementById("demo")
-    console.log(canvas)
-    var ctx = canvas.getContext("2d");
-    var img = new Image();
-    img.onload = function() {
-      ctx.drawImage(img, 10, 10, 200, 200)
-      ctx.beginPath()
-      ctx.moveTo(30, 96)
-      ctx.lineTo(70, 66)
-      ctx.lineTo(103, 76)
-      ctx.lineTo(170, 15)
-      ctx.stroke()
-    };
-    img.src = require("../../assets/logo.png")
-    demo.src = require("../../assets/logo.png")
+    let demo = document.getElementById("demo");
+    Axios.get("http://192.168.21.29:8099/poster/posters/page", {
+      params: {
+        seriesId: 1,
+        videoNumber: 1,
+        pageNum: 1,
+        pageSize: 1
+      }
+    })
+      .then(function(response) {
+        // console.log(response.data.rows[0].base64String)
+        demo.src =
+          "data:image/png;base64," + response.data.rows[0].base64String;
+        demo.style.width = response.data.rows[0].width;
+        demo.style.height = response.data.rows[0].height;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 };
 </script>
@@ -169,7 +206,7 @@ export default {
     img {
       width: 140px;
       height: 140px;
-      &.active{
+      &.active {
         border: 3px solid #22a2ff;
         border-radius: 6px;
       }
@@ -180,28 +217,28 @@ export default {
     font-size: 16px;
   }
 }
-.alter{
+.alter {
   color: red;
   font-size: 14px;
   box-sizing: border-box;
   display: inline-block;
   height: 30px;
 }
-.choose-size{
+.choose-size {
   display: inline-block;
   height: 100%;
-  .left-preview{
+  .left-preview {
     display: inline-block;
     width: 160px;
     background-color: #fff;
   }
-  .center-editor{
+  .center-editor {
     display: inline-block;
     height: 100%;
     width: 480px;
     background-color: red;
   }
-  .right-handle{
+  .right-handle {
     display: inline-block;
     width: 180px;
     height: 100%;
